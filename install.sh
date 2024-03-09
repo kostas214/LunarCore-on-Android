d1b2a41ff73035f162671b7d77f0f351426f54e6
kostas214 <100384475+kostas214@users.noreply.github.com>
2024-03-10 00:31:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-nix-env -iA nixpkgs.nano nixpkgs.mongodb nixpkgs.jdk17 nixpkgs.wget nixpkgs.unzip
+nix-env -iA nixpkgs.nano nixpkgs.mongodb nixpkgs.jdk17 nixpkgs.wget nixpkgs.unzip nixpkg.rsync
 wget https://nightly.link/Melledy/LunarCore/workflows/build/development/LunarCore.zip
 unzip LunarCore.zip
 rm LunarCore.zip
 mkdir resources
 git clone https://github.com/Dimbreath/StarRailData
 cd StarRailData
-mv ExcelOutput TextMap ~/LunarCore-on-Android/resources
+mv Config ExcelOutput TextMap ~/LunarCore-on-Android/resources
 cd ..
 rm -rf StarRailData
 git clone https://gitlab.com/Melledy/LunarCore-Configs
 cd LunarCore-Configs
-mv Config ~/LunarCore-on-Android/resources
+rsync -a Config ~/LunarCore-on-Android/resources
 cd ..
 rm -rf LunarCore-Configs
 cp start.sh ~/
