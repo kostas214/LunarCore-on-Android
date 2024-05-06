061af9140f6f434036ddf8fe45c843dd7d478c3d
Kostas214pro <100384475+kostas214@users.noreply.github.com>
2024-05-07 02:23:20 +0300

    Update hotFixGraber.py

--- hotFixGraber.py
+++ hotFixGraber.py
@@ -5,14 +5,15 @@ import re
 import json
 import sys
 
-dispatchSeed = "bba67d093e"
-version = "OSPRODWin2.1.0"
-url = f"https://prod-official-eur-dp01.starrails.com/query_gateway?version={version}&t=1713563129&uid=10004&language_type=3&platform_type=3&dispatch_seed={dispatchSeed}&channel_id=1&sub_channel_id=1&is_need_url=1&account_type=1&account_uid=307575403"
+dispatchSeed = "cd3cd8494d"
+version = "2.2"
+versionType= "Live"
+url = f"https://prod-official-eur-dp01.starrails.com/query_gateway?version=OSPRODWin{version}.0&t=1713563129&uid=10004&language_type=3&platform_type=3&dispatch_seed={dispatchSeed}&channel_id=1&sub_channel_id=1&is_need_url=1&account_type=1&account_uid=307575403"
 hotfixPatterns = {
-    'assetBundleUrl': r'https://autopatchos.starrails.com/asb/V2.1Live/[\w_-]+',
-    'exResourceUrl': r'https://autopatchos.starrails.com/design_data/V2.1Live/[\w_-]+',
-    'luaUrl':  r'https://autopatchos.starrails.com/lua/V2.1Live/[\w_-]+',
-    'ifixUrl': r'https://autopatchos.starrails.com/ifix/V2.1Live/[\w_-]+'
+    'assetBundleUrl': rf'https://autopatchos.starrails.com/asb/V{version}{versionType}/[\w_-]+',
+    'exResourceUrl': rf'https://autopatchos.starrails.com/design_data/V{version}{versionType}/[\w_-]+',
+    'luaUrl':  rf'https://autopatchos.starrails.com/lua/V{version}{versionType}/[\w_-]+',
+    'ifixUrl': rf'https://autopatchos.starrails.com/ifix/V{version}{versionType}/[\w_-]+'
 }
 
 if len(sys.argv) == 1:
@@ -24,6 +25,7 @@ while True:
         try:
             request = requests.get(url)
             config = base64.b64decode(request.content)
+            print(config)
             decode,messagetype = blackboxprotobuf.protobuf_to_json(config)
             for key, pattern in hotfixPatterns.items():
                 idk = re.findall(pattern, decode)
